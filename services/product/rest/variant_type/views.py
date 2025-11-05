a191b8f6b5226a87fd5b3cf1abaff4d467c5ad0b
herlyplanviu <herly@planviu.com>
2025-11-06 00:12:13 +0700

    Update views.py

--- services/product/rest/variant_type/views.py
+++ services/product/rest/variant_type/views.py
@@ -4,16 +4,17 @@ from django.utils.translation import gettext_lazy as _
 import logging
 from core.common.viewsets import BaseViewSet
 from services.product.models.variant_type import ProductVariantType
-from services.product.rest.variant_type.serializers import ProductVariantTypeSerializer, ProductVariantTypeSerializerSimple
+from services.product.rest.variant_type.serializers import (
+    ProductVariantTypeSerializer,
+    ProductVariantTypeSerializerSimple,
+)
 
 if TYPE_CHECKING:
     pass
 
 logger = logging.getLogger(__name__)
 
-__all__ = (
-    "ProductVariantTypeViewSet",
-)
+__all__ = ("ProductVariantTypeViewSet",)
 
 
 class ProductVariantTypeViewSet(BaseViewSet):
@@ -21,6 +22,7 @@ class ProductVariantTypeViewSet(BaseViewSet):
     A viewset for viewing and editing Product Variant Types.
     Accessible only by superusers.
     """
+
     queryset = ProductVariantType.objects.all()
     serializer_class = ProductVariantTypeSerializer
     lookup_field = "subid"
@@ -35,4 +37,15 @@ class ProductVariantTypeViewSet(BaseViewSet):
     serializer_map = {
         "autocomplete": ProductVariantTypeSerializerSimple,
     }
-    
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # Optional filter by product
+        product_subid = self.request.query_params.get("product")
+        if product_subid:
+            queryset = queryset.filter(
+                price_tiers__product__subid=product_subid
+            ).distinct()
+
+        return queryset
