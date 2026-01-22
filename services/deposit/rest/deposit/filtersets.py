3536cd3688e5c41215c23e43ca82508eca5a2f70
herlyplanviu <herly@planviu.com>
2026-01-22 17:32:35 +0700

    gitu kah

--- services/deposit/rest/deposit/filtersets.py
+++ services/deposit/rest/deposit/filtersets.py
@@ -4,14 +4,31 @@ from services.deposit.models import Deposit
 
 
 class DepositFilterSet(django_filters.FilterSet):
-    # ForeignKey filters (by PK)
     order = django_filters.CharFilter(field_name="order__subid")
     created_by = django_filters.CharFilter(
         field_name="created_by__subid", lookup_expr="exact"
     )
     is_paid_off = django_filters.BooleanFilter(field_name="is_paid_off")
-    is_expired = django_filters.BooleanFilter(field_name="is_expired")
+
+    is_expired = django_filters.BooleanFilter(method="filter_is_expired")
 
     class Meta:
         model = Deposit
         fields = ["order", "is_paid_off", "is_expired", "created_by"]
+
+    def filter_is_expired(self, queryset, name, value):
+        """
+        Expired means:
+        - is_expired = True
+        - AND is_paid_off = False
+        """
+        if value is True:
+            return queryset.filter(
+                is_expired=True,
+                is_paid_off=False,
+            )
+
+        if value is False:
+            return queryset.exclude(is_expired=False)
+
+        return queryset
