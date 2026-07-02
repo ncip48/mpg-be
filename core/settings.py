0b08c094cda0dc98b1dacc27465eab3a35760422
ncip48 <mbahcip00@gmail.com>
2026-07-02 07:49:39 +0700

    TZ

--- core/settings.py
+++ core/settings.py
@@ -151,12 +151,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "id"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Jakarta"
 
 USE_I18N = True
-
 USE_TZ = True
 
 
