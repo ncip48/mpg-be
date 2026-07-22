f6399b6b0c4f1c9b75812d4babed5a496f87cd27
ncip48 <mbahcip00@gmail.com>
2026-07-22 19:10:05 +0700

    fix

--- services/defect/rest/reject/utils.py
+++ services/defect/rest/reject/utils.py
@@ -1,23 +1,35 @@
 from services.defect.models import Reject
 from django.contrib.contenttypes.models import ContentType
 
+from django.contrib.contenttypes.models import ContentType
+
 def sync_reject(verification, qty, note, user):
+    content_type = ContentType.objects.get_for_model(verification)
+
+    reject = Reject.objects.filter(
+        content_type=content_type,
+        object_id=verification.pk,
+    ).first()
+
+    # Rules 1 & 2
     if verification.is_approved:
-        Reject.objects.filter(source=verification).delete()
+        if reject:
+            reject.delete()
         return
 
-    reject, created = Reject.objects.update_or_create(
-        content_type=ContentType.objects.get_for_model(verification),
-        object_id=verification.pk,
-        defaults={
-            "qty": qty,
-            "defect": note or "",
-        },
-    )
-
-    if created:
-        reject.created_by = user
-        reject.save(update_fields=["created_by"])
+    # Rules 3 & 4
+    if reject:
+        reject.qty = qty
+        reject.defect = note or ""
+        reject.save(update_fields=["qty", "defect"])
+    else:
+        Reject.objects.create(
+            content_type=content_type,
+            object_id=verification.pk,
+            qty=qty,
+            defect=note or "",
+            created_by=user,
+        )
         
         
 def sync_reject_manual(verification, qty, note, user):
